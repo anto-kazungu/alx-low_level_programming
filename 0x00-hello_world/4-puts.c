1bf790d7dc4d42251969db45089ee41558e36983
anto-kazungu <kazoga.antony@gmail.com>
2022-03-15 07:08:04 +0000

    First C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* puts is for printing text without formattins */
+
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+
+}
+
