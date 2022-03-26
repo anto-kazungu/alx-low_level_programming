986c2b5a77091f13d88e16e9f4f9459ce5d0ac83
anto-kazungu <kazoga.antony@gmail.com>
2022-03-26 13:32:36 +0000

    Variable and If else projects

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero", n);
+	}
+	else
+	{
+		printf("%d is negative", n);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+## Project folder for Variables, if else and while statements
+# Description
+All projects will be summarised here after completion
