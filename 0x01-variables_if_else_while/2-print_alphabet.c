4099642ac120d581d974d65479452af9e8fec465
anto-kazungu <kazoga.antony@gmail.com>
2022-03-26 13:55:39 +0000

    Printing alphabets in small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		printf("\n");
+	}
+
+	return (0);
+}
